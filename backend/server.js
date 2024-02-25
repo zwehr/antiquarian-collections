da7dac77639a78e652326bdc86d636f9235c1967
zwehr <zwehrmeister@gmail.com>
2024-02-24 19:32:41 -0500

    create basic/boilerplate book routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
+const bookRoutes = require('./routes/books');
 
 const app = express();
 
-app.listen(4000, () => {
-  console.log('listening on port 4000');
+// Middleware, currently just logging request path and method
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/books', bookRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT);
 });
