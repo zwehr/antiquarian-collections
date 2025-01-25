24c3812dbec482aa81eeb4947db5900250f7c4b8
zwehr <zwehrmeister@gmail.com>
2025-01-25 18:25:08 -0500

    create first get request

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
 app.get('/', (req, res) => {
@@ -7,6 +15,6 @@ app.get('/', (req, res) => {
 });
 
 // listen for requests
-app.listen(4000, () => {
-  console.log('listening on port 4000');
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT);
 });
