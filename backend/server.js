666c2eb0e0a0e72114129bacde7b402c6f575d81
zwehr <zwehrmeister@gmail.com>
2025-01-26 16:50:57 -0500

    add Mongoose and connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const bookRoutes = require('./routes/books');
 const app = express();
 
@@ -15,7 +16,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/books', bookRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
